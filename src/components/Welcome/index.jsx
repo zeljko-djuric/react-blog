b69ce552c5c5cc2346dd7d42e5b071c83edc0833
Zeljko Djuric <zeljko94@gmail.com>
2018-11-18 21:43:41 +0100

    Added reusable Banner component

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,31 +1,28 @@
 import React from 'react';
 import Article from './../Article'
+import Banner from './../Banner'
 
 const Welcome = () => {
     return (
-        <div>
-          <header className="header header-inverse" style={{backgroundImage: 'url(assets/img/bg-gift.jpg)'}} data-overlay={8}>
-    <div className="container text-center">
-    <div className="row">
-      <div className="col-12 col-lg-8 offset-lg-2">
-        <h1>Latest Blog Posts</h1>
-        <p className="fs-20 opacity-70">Read and get updated on how we progress.</p>
-      </div>
-    </div>
-  </div>
-</header>  
-<main className="main-content bg-gray">
-  <div className="row">
-      <div class="col-12 col-lg-6 offset-lg-3">
-        <Article/>
-        <hr/>
-        <Article/>
-        <hr/>
-        <Article/>
-      </div>
-  </div>
+        <div> 
+          <Banner
+            backgroundImage="url(assets/img/bg-gift.jpg)"
+            title="Latest Blog Post"
+            subTitle="Read and get updated on how we progress. "
+          />
 
-</main>  
+        <main className="main-content bg-gray">
+        <div className="row">
+        <div class="col-12 col-lg-6 offset-lg-3">
+        <Article/>
+        <hr/>
+        <Article/>
+        <hr/>
+        <Article/>
+        </div>
+        </div>
+
+        </main>  
         </div>
     );
 }
